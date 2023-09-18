609fb91906571c0f19266e08f8e1dba4e2fbee5c
yangjinping1 <yangjinping1@tal.com>
2023-09-18 18:25:22 +0800

    添加yolov5 trt推理

--- yolov5/cpp/run.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build
-cd build
-cmake ..
-make -j 16
-
-export LD_LIBRARY_PATH=/home/qfs/package_manager/qfs_3rdParty/opencv/linux/4.2.0/lib:/home/qfs/package_manager/TensorRT-8.4.1.5/lib/:$LD_LIBRARY_PATH
-./demo 
